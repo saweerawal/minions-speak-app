bb5ae1d005c2ec6599224aa91a73036bdaa3eaf5
Sawee Rawal <saweerawal1998@gmail.com>
2021-09-12 23:50:46 +0530

    fixed the indentation

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-var inputText  = document.querySelector("#input-area");
-var btn        = document.querySelector("#trans-btn");
+var inputText = document.querySelector("#input-area");
+var btn = document.querySelector("#trans-btn");
 var outputText = document.querySelector("#output");
 
 
-function contructUrl(text){
-    var server = "https://api.funtranslations.com/translate/minion.json" + "?text=" + text ;
+function contructUrl(text) {
+    var server = "https://api.funtranslations.com/translate/minion.json" + "?text=" + text;
     return server;
 
 }
 
-function tanslator(event){
+function tanslator(event) {
     var text = inputText.value;
     var url = contructUrl(text);
     fetch(url).then(response => response.json()).then(json => {
-     outputText.innerText =  json.contents.translated;
- })
- .catch(() => alert("error occured"));
+        outputText.innerText = json.contents.translated;
+    }).catch(() => alert("error occured"));
 
 
 }
